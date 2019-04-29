004b3461b0a1d50fddd2558aa9ad907273d0f05c
Antoni Żewierżejew <azewierzejew@gmail.com>
2019-04-29 08:28:50 +0200

    First docs.

--- src/find_route.h
+++ src/find_route.h
@@ -3,12 +3,19 @@
 
 #include "map_basics.h"
 
-
+/**
+ * Struktura przechowująca łączny dystans dla drogi.
+ */
 typedef struct DistanceStruct Distance;
 
+/**
+ * Struktura przechowująca wynik szukania drogi.
+ */
 typedef struct RouteSearchAnswerStruct RouteSearchAnswer;
 
-
+/**
+ * Definicja struktury
+ */
 struct DistanceStruct {
     uint64_t length;
     int lastRepaired;
@@ -24,4 +31,4 @@ int compareDistances(Distance distance1, Distance distance2);
 
 RouteSearchAnswer findRoute(Map *map, City *city1, City *city2, Vector *usedRoads);
 
-#endif //DROGI_FIND_ROUTE_H
+#endif /* DROGI_FIND_ROUTE_H */
