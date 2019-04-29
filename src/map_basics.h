439f4e4b4ae5893502120eeb8c6dd20ccd1a31a2
azewierzejew <azewierzejew@gmail.com>
2019-04-29 17:26:31 +0200

    Docs for map_basics.h.

--- src/map_basics.h
+++ src/map_basics.h
@@ -1,3 +1,10 @@
+/** @file
+ * Ogólne definicje związane z działaniem modułu map.
+ *
+ * @author Antoni Żewierżejew <azewierzejew@gmail.com>
+ * @date 29.03.2019
+ */
+
 #ifndef DROGI_MAP_BASICS_H
 #define DROGI_MAP_BASICS_H
 
@@ -10,40 +17,70 @@
 
 /* Definicje typów. */
 
+/** Struktura przechowująca mapę dróg krajowych. */
 typedef struct Map Map;
+
+/** Struktura przechowująca informacje o odcinku drogowym. */
 typedef struct RoadStruct Road;
+
+/** Struktura przechowująca informacje o mieście. */
 typedef struct CityStruct City;
+
+/** Struktura przechowująca informacje o drodze krajowej. */
 typedef struct RouteStruct Route;
 
 
 /* Deklaracje struktur. */
 
-struct RoadStruct {
-    int lastRepaired;
-    City *end1;
-    City *end2;
-    unsigned length;
-};
-
-struct CityStruct {
-    char *name;
-    Vector *roads;
-    size_t id;
-};
-
-struct RouteStruct {
-    City *end1;
-    City *end2;
-    Vector *roads;
-};
-
+/** Przechowuje elementy mapy. */
 struct Map {
+    /** Słownik, gdzie nazwie miasta jest przypisany wskaźnik na obiekt miasta. */
     Dict *cities;
+    /** Wskaźnik na tablicę wskaźników na drogi krajowe. */
     Route **routes;
+    /** Liczba miast na mapie. */
     size_t cityCount;
 };
 
+/** Przechowuje informacje o drodze. */
+struct RoadStruct {
+    /** Rok ostatniego remontu lub budowy. Jeśli jest @p 0 to droga była usunięta w jednym końcu. */
+    int lastRepaired;
+    /** Pierwszy koniec drogi. */
+    City *end1;
+    /** Drugi koniec drogi. */
+    City *end2;
+    /** Długość drogi. Jeśli jest @p 0 to droga jest niedostępna. */
+    unsigned length;
+};
 
+/** Przechowuje informacje o mieście. */
+struct CityStruct {
+    /** Wskaźnik na napis reprezentujący nazwę miasta. */
+    char *name;
+    /** Wskaźnik na wektor dróg wychodzących z miasta. */
+    Vector *roads;
+    /** Id miasta w danej mapie. */
+    size_t id;
+};
+
+/** Przechowuje informacje o dordze krajowej. */
+struct RouteStruct {
+    /** Początek drogi. */
+    City *end1;
+    /** Koniec drogi. */
+    City *end2;
+    /** Wektor kolejnych odcinków drogowych. */
+    Vector *roads;
+};
+
+
+/**
+ * Wyznacza jaki jest drugi koniec odcinka drogowego.
+ * @param[in] road - początek
+ * @param[in] end
+ * @return
+ */
 City *otherRoadEnd(Road *road, City *end);
 
 void doNothing(void *arg);
