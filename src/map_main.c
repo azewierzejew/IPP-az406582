9af2b1df8208c1fac0fb87d55bbd36b9deaa456a
Antoni Żewierżejew <azewierzejew@gmail.com>
2019-05-15 17:17:06 +0200

    map_main.c progress

--- src/map_main.c
+++ src/map_main.c
@@ -10,7 +10,7 @@
 #include <inttypes.h>
 #include <stdbool.h>
 
-char *getNextParameter(char *s) {
+static char *getNextParameter(char *s) {
     static char *savePointer = NULL;
     if (s != NULL) {
         if (*s == '\0') {
@@ -35,9 +35,31 @@ char *getNextParameter(char *s) {
     return s;
 }
 
+static bool stringToUnsigned(const char *str, unsigned *number) {
+    char dump[2];
+    if (sscanf(str, "%u%1s", number, dump) != 1) {
+        return false;
+    }
+
+    return true;
+}
+
+static bool stringToInt(const char *str, int *number) {
+    char dump[2];
+    if (sscanf(str, "%d%1s", number, dump) != 1) {
+        return false;
+    }
+
+    return true;
+}
 
 static bool executeCommand(char *command, size_t len) {
     if (command == NULL || len == 0) {
+        return false;
+    }
+
+    if (len != strlen(command)) {
+        /* Wczytano zerowy bajt, który jest niepoprawny. */
         return false;
     }
 
@@ -47,22 +69,42 @@ static bool executeCommand(char *command, size_t len) {
     }
     command[len - 1] = '\0';
 
-    if (command[0] == '#' || len == 1) {
+    if (command[0] == '#') {
         return true;
     }
 
-    Vector *params = initVector();
-    if (params == NULL) {
-        return false;
+    Vector *parametersVector = initVector();
+    if (parametersVector == NULL) {
+        goto errorBase;
     }
 
     char *nextParameter = getNextParameter(command);
     while (nextParameter != NULL) {
-        if (!pushToVector(params, nextParameter)) {
-            deleteVector(params, doNothing);
+        if (!pushToVector(parametersVector, nextParameter)) {
+            deleteVector(parametersVector, doNothing);
+            return false;
         }
         nextParameter = getNextParameter(NULL);
     }
+
+    size_t parameterCount = sizeOfVector(parametersVector);
+    const char **parameters = storageBlockOfVector(parametersVector);
+    if (parameterCount == 0) {
+        return true;
+    }
+
+    if (strcmp(parameters[0], "addRoad") == 0) {
+        const char *city1Name = parameters[1];
+        const char *city2Name = parameters[2];
+        unsigned length;
+        if (!stringToUnsigned(parameters[3], &length)) {
+
+        }
+    }
+
+    deleteVector(parametersVector, doNothing);
+    errorBase:
+    return false;
 }
 
 int main() {
@@ -74,12 +116,14 @@ int main() {
     char *buff = NULL;
     size_t len = 0;
     uint64_t lineNumber = 0;
+    ssize_t readLength = getline(&buff, &len, stdin);
 
-    while (getline(&buff, &len, stdin) != -1) {
+    while (readLength >= 0) {
         lineNumber++;
-        if (!executeCommand(buff, len)) {
+        if (!executeCommand(buff, readLength)) {
             fprintf(stderr, "ERROR %"PRIu64"\n", lineNumber);
         }
+        readLength = getline(&buff, &len, stdin);
     }
 
     free(buff);
